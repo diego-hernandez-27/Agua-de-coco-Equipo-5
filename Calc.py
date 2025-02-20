caefdece2c9865991bffea4225c4d30306586be8
Diego Alberto Hernandez Reyes <dhernandezr1906@alumno.ipn.mx>
2025-02-20 15:55:26 -0600

    Menu y suma

---
+++ Calc.py
@@ -0,0 +1,30 @@
+def suma(a, b):
+    return a + b
+
+def __main__():
+    print('Hola queridísimo usuario, bienvenido a la calculadora')
+    bandera = True
+
+    while bandera:
+        print('Elija una opción de la siguiente lista: ')
+        print('1. Suma')
+        print('2. Resta')
+        print('3. Multiplicación')
+        print('4. División')
+        print('5. Potencia')
+        print('6. Factorial')
+        print('7. Salir')
+
+        opcion = int(input('Ingrese su opción: '))
+
+        if opcion == 1:
+            a = int(input('Ingrese el primer número: '))
+            b = int(input('Ingrese el segundo número: '))
+            print('Resultado: ' + str(suma(a, b)))
+        elif opcion == 7:
+            bandera = False
+        else:
+            print('Opción no válida')
+
+if __name__ == "__main__":
+    __main__()
